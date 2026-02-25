2e182e8dcb1ef25fbc9d268edc9d7a99828c778c
admin <admin>
2026-02-25 15:08:37 -0800

    Updating the main branch

--- my_users.py
+++ my_users.py
@@ -6,7 +6,6 @@ users = client.list_users()
 rows = []
 index = 0
 for u in users:
-    profile = u.get("userProfile")
     login = u["login"]
     user_obj = client.get_user(login)
     activity = user_obj.get_activity().get_raw()
@@ -14,7 +13,6 @@ for u in users:
         "index": index
         "login": login,
         "display_name": u["displayName"],
-        "profile": profile,
         "last_successful_login": activity["lastSuccessfulLogin"],
         "last_failed_login": activity["lastFailedLogin"],
         "last_session_activity": activity["lastSessionActivity"]
